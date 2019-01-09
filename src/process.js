4f78374a8520a2fe31bde5f6f57565b37c9106f1
d-smith <doug.smith.mail@gmail.com>
2019-01-09 14:56:33 -0800

    Added all steps and choices

--- src/process.js
+++ src/process.js
@@ -5,3 +5,32 @@ module.exports.step1 = async (event, context) => {
     
     return event;
 }
+
+const makeNamedStep = (name) => {
+    return async (event, context) => {
+        //Case data via middleware
+        console.log(name);
+
+        //Add step output to case data.
+        let caseData = event.caseData || {};
+        caseData[name] = name + ' output'; //step output
+
+        //Return case data and state machine data
+        let stateMachineData = {
+            processData: event['processData']
+        };
+        return { caseData, stateMachineData };
+    };
+}
+
+module.exports.fooStep
+    = makeNamedStep('foo');
+
+module.exports.barStep
+    = makeNamedStep('bar');
+
+module.exports.bazStep
+    = makeNamedStep('baz');
+
+module.exports.quuxStep
+    = makeNamedStep('quux');
